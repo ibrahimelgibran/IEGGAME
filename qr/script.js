68a46f77f122069d550ab9a82e1244da415afefc
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-05-26 13:35:55 +0700

    update

---
+++ qr/script.js
@@ -0,0 +1,80 @@
+const container = document.querySelector(".container");
+const userInput = document.getElementById("userInput");
+const submitBtn = document.getElementById("submit");
+const downloadBtn = document.getElementById("download");
+const sizeOptions = document.querySelector(".sizeOptions");
+const BGColor = document.getElementById("BGColor");
+const FGColor = document.getElementById("FGColor");
+let QR_Code;
+let sizeChoice = 100,
+  BGColorChoice = "#ffffff",
+  FGColorChoice = "#377dff";
+
+//Set size
+sizeOptions.addEventListener("change", () => {
+  sizeChoice = sizeOptions.value;
+});
+
+//Set background color
+BGColor.addEventListener("input", () => {
+  BGColorChoice = BGColor.value;
+});
+
+//Set foreground color
+FGColor.addEventListener("input", () => {
+  FGColorChoice = FGColor.value;
+});
+
+//Format input
+const inputFormatter = (value) => {
+  value = value.replace(/[^a-z0-9A-Z]+/g, "");
+  return value;
+};
+
+submitBtn.addEventListener("click", async () => {
+  container.innerHTML = "";
+  //QR code generation
+  QR_Code = new QRCode(container, {
+    text: userInput.value,
+    width: sizeChoice,
+    height: sizeChoice,
+    colorDark: FGColorChoice,
+    colorLight: BGColorChoice,
+  });
+
+  // Wait for the QR code to be rendered
+  setTimeout(() => {
+    //Set url for download
+    const src = container.firstChild.toDataURL("image/png");
+    downloadBtn.href = src;
+    let userValue = userInput.value;
+    try {
+      userValue = new URL(userValue).hostname;
+    } catch (_) {
+      userValue = inputFormatter(userValue);
+    }
+    downloadBtn.download = `${userValue}QR.png`;
+    downloadBtn.classList.remove("hide");
+  }, 300); // Adjust the timeout as necessary
+});
+
+userInput.addEventListener("input", () => {
+  if (userInput.value.trim().length < 1) {
+    submitBtn.disabled = true;
+    downloadBtn.href = "";
+    downloadBtn.classList.add("hide");
+  } else {
+    submitBtn.disabled = false;
+  }
+});
+
+window.onload = () => {
+  container.innerHTML = "";
+  sizeChoice = 100;
+  sizeOptions.value = 100;
+  userInput.value = "";
+  BGColor.value = BGColorChoice = "#ffffff";
+  FGColor.value = FGColorChoice = "#377dff";
+  downloadBtn.classList.add("hide");
+  submitBtn.disabled = true;
+};
